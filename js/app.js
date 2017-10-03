266bdde660daf06c785d0a37f263006b116ec3d6
egomadking <ego.madking@gmail.com>
2017-10-02 21:24:25 -0400

    jQuery TOC move for projectNav, scrollfix for anchor + fixed header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(document).foundation()
+$(document).foundation();
+
+//append #markdown-toc to #projectNav, add Foundation details
+$("#markdown-toc").detach().appendTo("#projectNav");
+$("#markdown-toc").addClass("vertical menu accordion-menu");
+$("#markdown-toc").data("data-accordion-menu");
+
+//need to id sub-menu items "menu vertical nested"
+//https://foundation.zurb.com/sites/docs/accordion-menu.html
+
+//anchor fix for #projectNav links + fixed header
+var shiftWindow = function() { scrollBy(0, -60); };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
