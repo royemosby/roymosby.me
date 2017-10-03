89674c616594517e69bdac465915c9469df7ecbe
egomadking <ego.madking@gmail.com>
2017-10-03 08:06:51 -0400

    nested projectNav menu started

--- js/app.js
+++ js/app.js
@@ -8,7 +8,12 @@ $("#markdown-toc").data("data-accordion-menu");
 //need to id sub-menu items "menu vertical nested"
 //https://foundation.zurb.com/sites/docs/accordion-menu.html
 
+$("#markdown-toc > li").addClass("projectHeading");
+$(".projectHeading > ul").addClass("menu vertical nested");
+
 //anchor fix for #projectNav links + fixed header
-var shiftWindow = function() { scrollBy(0, -60); };
+var shiftWindow = function () {
+  scrollBy(0, -60);
+};
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
