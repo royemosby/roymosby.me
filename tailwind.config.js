d76ed43163014bed9d823eefd7ff5c1da3d019bc
Roy Mosby <ego.madking@gmail.com>
2020-12-17 21:40:49 -0500

    install postcss, tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    'src/**/*.html',
+    'src/**/*.liquid',
+    'src/**/*.md'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
