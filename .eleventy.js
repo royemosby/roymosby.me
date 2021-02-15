4cef2f211b4dc39699a3c9a99c93cc827f3c3419
Roy Mosby <ego.madking@gmail.com>
2021-02-14 22:00:45 -0500

    add syntaxHighlight eleventy plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig){
 
   eleventyConfig.setDataDeepMerge(true);
@@ -6,6 +8,7 @@ module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/styles');
   eleventyConfig.addPassthroughCopy('src/scripts');
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir:{
