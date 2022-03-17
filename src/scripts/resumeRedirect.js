c57765a121ae2446ea811ddf13d27eecf5e11ce8
Roy Mosby <ego.madking@gmail.com>
2022-03-17 12:38:04 -0400

    adds resume redirect page

---
+++ src/scripts/resumeRedirect.js
@@ -0,0 +1,6 @@
+window.onload = ()=> {
+  setTimeout(()=> {
+    location.href = "https://docs.google.com/document/d/1tM4cT3xsEtXB3lQHkqGLipbEr8d4UJ1vQckMLVDeKP4/edit?usp=sharing"
+
+  }, 1500);
+};
