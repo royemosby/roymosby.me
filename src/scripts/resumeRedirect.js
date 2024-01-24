803fecf509c4cd6ea9b104c729c627dd80c7eac6
Roy Mosby <ego.madking@gmail.com>
2024-01-24 12:39:33 -0500

    bootstrap astro wip

--- src/scripts/resumeRedirect.js
+++
@@ -1,6 +0,0 @@
-window.onload = ()=> {
-  setTimeout(()=> {
-    location.href = "https://docs.google.com/document/d/1tM4cT3xsEtXB3lQHkqGLipbEr8d4UJ1vQckMLVDeKP4/edit?usp=sharing"
-
-  }, 1500);
-};
